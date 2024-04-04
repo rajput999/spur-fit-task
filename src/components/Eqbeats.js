336d0b235a7d8ab85aee797c78ec2f0d935a3c2c
rajput999 <rajput9997647419@gmail.com>
2024-04-04 18:35:07 +0530

    eq_beats

---
+++ src/components/Eqbeats.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from '../styles/eqbeats.module.css'
+
+
+const Eqbeats = () => {
+  return (
+    <div className={styles.eqbeat}>
+            <h3>EQ beats IQ</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo reiciendis fugit molestiae? Et, exercitationem nihil placeat ea eum distinctio adipisci!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, facilis expedita consectetur repellendus quam vitae?</p>
+        </div>
+  )
+}
+
+export default Eqbeats
