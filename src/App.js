73e7c9fbfc816a99d412d24e2ed0580815045cc7
rajput999 <rajput9997647419@gmail.com>
2024-04-06 11:31:33 +0530

    Work with us

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Section6 from './components/Section6';
 import Section7 from './components/Section7';
 import Section8 from './components/Section8';
 import Section9 from './components/Section9';
+import Section10 from './components/Section10';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
       <Section7/>
       <Section8 />
       <Section9 />
+      <Section10/>
     </div>
   );
 }
---
+++ src/components/Section10.js
@@ -0,0 +1,92 @@
+import React, { useEffect } from 'react';
+import styles from "../styles/section10.module.css";
+import happyblob from "../images/happyblob.png";
+import cardListData from '../data/workWithUs.json'
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+const Section10 = () => {
+
+    useEffect(() => {
+        const box = document.querySelector(`.${styles.sectionTitle}`);
+
+        console.log(`.${styles.sec10Title}`)
+        var t1 = gsap.timeline({
+            scrollTrigger: {
+                trigger: `#companyName`,
+                start: "0 70%",
+                end: "0px 70%",
+                toggleActions: "restart none reset reverse",
+            },
+        });
+        t1.fromTo(
+            `#companyName`,
+            { x: +200, opacity: 0 },
+            { x: -40, opacity: .8, duration: 0.6 }
+        );
+        t1.to(`#companyName`, { x: 0, duration: 0.3, opacity: 1 });
+        var t2 = gsap.timeline({
+            scrollTrigger: {
+                trigger: `#companyName`,
+                start: "0 70%",
+                end: "0px 70%",
+                toggleActions: "restart none reset reverse",
+            },
+        });
+        t2.fromTo(
+            ` #sectionTitle`,
+            { x: -200, opacity: 0 },
+            { x: +40, opacity: .8, duration: 0.6 }
+        );
+        t2.to(`#sectionTitle`, { x: 0, duration: 0.3, opacity: 1 });
+        var t3 = gsap.timeline({
+            scrollTrigger: {
+                trigger: `#sec10Content img`,
+                start: "0 90%",
+                end: "0px 90%",
+                toggleActions: "restart none reset reverse",
+            },
+        });
+        t3.fromTo(
+            `#sec10Content img`,
+            { rotateZ: 30, scale: .9, transformOrigin: "50% 20%" },
+            { rotateZ: -30, scale: 1.2, duration: 0.6, repeat: -1, yoyo: "true", }
+        );
+    }, []);
+
+    return (
+        <div className={styles.section10}>
+            <div className={styles.sec10Title}>
+                <h2 id="sectionTitle">Work with us</h2>
+                <h2 id="companyName">ahead</h2>
+            </div>
+
+            <div className={styles.sec10Content} id="sec10Content">
+                <div className={styles.sec10Info}>
+                    <div className={styles.sec10About}>
+                        <img src={happyblob} alt="" />
+                        <div style={{fontSize:'1.5rem',fontWeight:'700',margin:'0.6rem 0'}}>About</div>
+                        <span>At ahead our goal is to make self-improvement fun and lasting. We know there's a way how to make it work. And that's what aHead is all about!</span>
+                    </div>
+                    <div className={styles.sec10Product}>
+                        <div style={{fontSize:'1.5rem',fontWeight:'700',margin:'0.6rem 0'}}>Product</div>
+                        <span>Sure, you could spend ages reading books or sitting in seminars on how to become a better spouse, parent, or manager - like we did...</span>
+                    </div>
+                </div>
+                <div className={styles.sec10Scroll}>
+                    {cardListData && cardListData.map((card, i) => {
+                        return (
+                            <div key={i} className={styles.sec10ScrollItem}>
+                                <h6>{card.title}</h6>
+                                <span>{card.text}</span>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Section10;
